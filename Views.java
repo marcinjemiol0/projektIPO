bf300551b2e6ebaabcca05be142a78c5c8d278e3
marcinjemiol0 <marcinjemiolo@o2.pl>
2017-06-27 11:03:20 +0200

    Dodanie nowego komentarza o klasie Views

--- Views.java
+++ Views.java
@@ -5,7 +5,7 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 
-
+//tworzenie klasy glownej views, ktora uruchomi klase LogIn
 public class Views extends Application {
 
     
